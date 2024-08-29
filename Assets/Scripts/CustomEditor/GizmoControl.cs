68517e1a3d99dba00ac028fc8a0e96fe62854c4f
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2024-08-29 22:02:06 +0200

    started migrating object control to gizmo

---
+++ Assets/Scripts/CustomEditor/GizmoControl.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Axis
+{
+	NONE,
+	X,
+	Y,
+	Z
+}
+
+public class GizmoControl : MonoBehaviour
+{
+	protected FreeEditorCam editorCam;
+
+	Vector2 initMousePos;
+	bool mousePressed;
+	Vector2 oldDelta;
+	protected Vector2 mouseDelta;
+
+	public bool wasSelected;
+	public bool isSelected;
+
+	public Transform[] xControl, yControl, zControl;
+	public LayerMask gizmoLayer;
+	public GameObject affectedObject;
+	private CurrentAxis currentAxis;
+
+	protected virtual void Start()
+	{
+		editorCam = FindObjectOfType<FreeEditorCam>();
+		ObjectRegister.Instance.RefreshMode();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			mousePressed = false;
+			currentAxis = null;
+		}
+
+		if (mousePressed)
+		{
+
+			Vector2 newDelta = new Vector2(-Mathf.RoundToInt(initMousePos.x - Input.mousePosition.x) / 100, -Mathf.RoundToInt(initMousePos.y - Input.mousePosition.y) / 100);
+			if (oldDelta != newDelta)
+			{
+				mouseDelta = newDelta;
+				if (currentAxis != null)
+					ObjectModification(currentAxis.axis);
+			}
+			else
+			{
+				mouseDelta = Vector2.zero;
+			}
+
+			oldDelta = newDelta;
+
+			if (mouseDelta.x >= 1 || mouseDelta.y >= 1 || mouseDelta.x <= -1 || mouseDelta.y <= -1)
+			{
+				initMousePos = Input.mousePosition;
+				mouseDelta = Vector2.zero;
+			}
+		}
+
+		RaycastHit hit;
+		Vector3 mouse = Input.mousePosition;
+		Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+
+		if (Physics.Raycast(castPoint, out hit, 100, gizmoLayer) && (Input.GetMouseButtonDown(0)) /*&& wasSelected == isSelected*/)
+		{
+			Debug.Log("shoot raycast");
+
+			if (GetHitAxis(hit) == Axis.NONE)
+			{
+				Debug.Log("nope");
+
+				return;
+			}
+
+			if (Input.GetMouseButtonDown(0))
+			{
+				initMousePos = Input.mousePosition;
+				mousePressed = true;
+			}
+
+			Axis _axis = GetHitAxis(hit);
+
+			switch (_axis)
+			{
+				case Axis.NONE:
+					break;
+				case Axis.X:
+					currentAxis = new CurrentAxis(Vector3.right);
+					break;
+				case Axis.Y:
+					currentAxis = new CurrentAxis(Vector3.up);
+					break;
+				case Axis.Z:
+					currentAxis = new CurrentAxis(Vector3.forward);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	private Axis GetHitAxis(RaycastHit hit)
+	{
+		foreach (Transform control in xControl)
+		{
+			if (hit.transform == control)
+				return Axis.X;
+		}
+
+		foreach (Transform control in yControl)
+		{
+			if (hit.transform == control)
+				return Axis.Y;
+		}
+
+		foreach (Transform control in zControl)
+		{
+			if (hit.transform == control)
+				return Axis.Z;
+		}
+
+		return Axis.NONE;
+	}
+
+	protected virtual void ObjectModification(Vector3 vectorDirection)
+	{
+
+	}
+}
+
+[System.Serializable]
+public class CurrentAxis
+{
+	public Vector3 axis;
+
+	public CurrentAxis(Vector3 axis)
+	{
+		this.axis = axis;
+	}
+}
---
+++ Assets/Scripts/CustomEditor/GizmoControl/PositionGizmo.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionGizmo : GizmoControl
+{
+	protected override void ObjectModification(Vector3 axis)
+	{
+		int vectorDir = 0;
+		int usedMouseDelta = 1;
+
+		if (mouseDelta.x == 1 || mouseDelta.y == 1)
+			usedMouseDelta = 1;
+		else if (mouseDelta.x == -1 || mouseDelta.y == -1)
+			usedMouseDelta = -1;
+		else
+			usedMouseDelta = 0;
+
+		int camRotx = 1;
+		int camRotz = 1;
+		if (editorCam.parentTransform.transform.eulerAngles.y < 270 && editorCam.parentTransform.transform.eulerAngles.y > 90)
+			camRotx = -1;
+		if (editorCam.parentTransform.transform.eulerAngles.y > 180)
+			camRotz = -1;
+
+		if (axis == Vector3.up) vectorDir = 1;
+		else if (axis == Vector3.right) vectorDir = 1;
+		else if (axis == Vector3.forward) vectorDir = -1;
+
+		if (Input.GetMouseButton(0))
+		{
+			Vector3 originalePosition = affectedObject.transform.position;
+			Vector3 tempPosition = originalePosition + new Vector3(axis.x * usedMouseDelta * vectorDir * camRotx, axis.y * usedMouseDelta * vectorDir, axis.z * usedMouseDelta * vectorDir * camRotz);
+
+			affectedObject.transform.position = tempPosition;
+		}
+	}
+}
